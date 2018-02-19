fcec8387db29c3ab11e23cb79595e38e992e00d6
Jack D <dmarkov23@gmail.com>
2018-02-19 22:15:53 +0100

    setup of basic GUI layout

--- automata-and-logic-engineering-client/src/app/app.module.ts
+++ automata-and-logic-engineering-client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {FormulaComponent} from './components/formula/formula.component';
 import {AssignmentResultService} from "./services/assignment-result.service";
+import { TabBarComponent } from './components/tab-bar/tab-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormulaComponent
+    FormulaComponent,
+    TabBarComponent
   ],
   imports: [
     HttpClientModule,
