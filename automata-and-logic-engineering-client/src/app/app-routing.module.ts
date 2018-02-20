ca4026de619c47c565dddbfeff249689108bf131
Jack D <dmarkov23@gmail.com>
2018-02-20 23:24:34 +0100

    finished setup of router

--- automata-and-logic-engineering-client/src/app/app-routing.module.ts
+++ automata-and-logic-engineering-client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {CalculationResultComponent} from "./components/calculation-result/calculation-result.component";
+
+
+const routes: Routes = [
+  {path: 'assignment1', component: CalculationResultComponent}
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
+
 }
