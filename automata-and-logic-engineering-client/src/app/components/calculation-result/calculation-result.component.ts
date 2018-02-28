ac2556001654766804ba974f85007fcdfb9aee0b
Jack D <dmarkov23@gmail.com>
2018-02-28 22:34:29 +0100

    setup of calculation result automatically displayed in the html

--- automata-and-logic-engineering-client/src/app/components/calculation-result/calculation-result.component.ts
+++ automata-and-logic-engineering-client/src/app/components/calculation-result/calculation-result.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-calculation-result',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculationResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dataService: DataService) {
+  }
 
   ngOnInit() {
   }
